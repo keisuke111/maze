815b461d664706f5bf5e290ebe2f170551b7cbc6
keisuke111 <kesk.k99@gmail.com>
2018-01-20 15:55:58 +0900

    del working.md

--- js/maze.js
+++ js/maze.js
@@ -1,7 +1,6 @@
 // 棒があるか確認
 const isExistsWall = (ary, y, x) => ary[y][x] == 1 ? false : true
 
-
 // 表示
 const printMaze = ary => {
 
@@ -9,7 +8,7 @@ const printMaze = ary => {
     section.id = "maze"
 
     document.body.appendChild(section)
-    
+
     ary.map(x => {
         let row = document.createElement("div");
         row.id = "maze-row"
@@ -28,14 +27,14 @@ const printMaze = ary => {
 
             row.appendChild(part)
         })
-        
+
         section.appendChild(row);
     })
 }
 
 // 迷路作成
 const createMaze = size => {
-  
+
     // 初期状態の作成
     // 1:壁
     // 0:道
@@ -43,10 +42,10 @@ const createMaze = size => {
     // 3:ゴール
 
     let mazeAry = (
-        [...Array(15)].map(x => [...Array(15)].map(x => 0))
+        [...Array(size)].map(x => [...Array(size)].map(x => 0))
     )
-        .map((x, i) => 
-            x.map((y, j) => 
+        .map((x, i) =>
+            x.map((y, j) =>
                 i == 0 || i == size - 1 || j == 0 || j == size - 1 ? 1 // 外壁
               : i % 2 == 0 && j % 2 == 0                           ? 1 // 内壁
               : i == 1 && j == 1                                   ? 2 // スタート
@@ -60,14 +59,14 @@ const createMaze = size => {
     for (let i = 2; i < size - 2; i += 2) {
         for (let j = 2; j < size - 2; j += 2) {
 
-            const rm = i == 2 ? Math.floor(Math.random() * 4) 
+            const rm = i == 2 ? Math.floor(Math.random() * 4)
                      :          Math.floor(Math.random() * 3) + 1
 
             rm == 0 ? (mazeAry[i - 1][j] = 1)
-          : rm == 1 ? 
+          : rm == 1 ?
               isExistsWall(mazeAry, i + 1, j) ? (mazeAry[i + 1][j] = 1)
             :                                   (j -= 2)
-          : rm == 2 ? 
+          : rm == 2 ?
               isExistsWall(mazeAry, i, j + 1) ? (mazeAry[i][j + 1] = 1)
             :                                   (j -= 2)
           : rm == 3 ?
@@ -82,7 +81,7 @@ const createMaze = size => {
 }
 
 window.onload = _ => {
-    let map = createMaze(15);
+    let map = createMaze(33);   // 5以上の奇数
     printMaze(map);
     let answered = false;
 }
