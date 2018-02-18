5dcf536f0dad7d8fa4696eee5f1aaec3885ce2d6
keisuke111 <kesk.k99@gmail.com>
2018-02-18 15:05:53 +0900

    update

--- js/maze.js
+++ js/maze.js
@@ -1,49 +1,52 @@
-const MAZE_ARRAY = createMaze(MAZE_SIZE)
+window.onload = _ => {
 
-let minStep = Number.MAX_VALUE
-let minRoute
+    const MAZE_ARRAY = createMaze(MAZE_SIZE)
 
-// Q値の初期化
-let qAry = createQ(MAZE_SIZE, Q_MAX)
+    let minStep = Number.MAX_VALUE
+    let minRoute
 
-for (let i = 0; i < MAX_LEANING; i++) {
-    // エージェントの初期化
-    let agent = {
-        x: 1,
-        y: 1,
-        nextX: 1,
-        nextY: 1,
-        step: 0,
-        route: []
-    }
+    // Q値の初期化
+    let qAry = createQ(MAZE_SIZE, Q_MAX)
 
-    let isGoal = false;
-
-    while (!isGoal) {
-        agent.step++;
-
-        let action = eGreedy(qAry, agent, EPSILON)
-        let reward = calcReward(action, agent, MAZE_ARRAY, HIT_WALL_POINT, GOAL_POINT)
-        reward += STEP_POINT
-        agent.route.push([agent.nextX, agent.nextY])
-        updateQ(action, reward, qAry, agent, ALPHA, GAMMA)
-
-        // 現在地の更新
-        agent.x = agent.nextX
-        agent.y = agent.nextY
-
-        // ゴール判定
-        if (agent.x == MAZE_SIZE - 2 && agent.y == MAZE_SIZE - 2) {
-            isGoal = true
+    for (let i = 0; i < MAX_LEANING; i++) {
+        // エージェントの初期化
+        let agent = {
+            x: 1,
+            y: 1,
+            nextX: 1,
+            nextY: 1,
+            step: 0,
+            route: []
         }
+
+        let isGoal = false;
+
+        while (!isGoal) {
+            agent.step++;
+
+            let action = eGreedy(qAry, agent, EPSILON)
+            let reward = calcReward(action, agent, MAZE_ARRAY, HIT_WALL_POINT, GOAL_POINT)
+            reward += STEP_POINT
+            agent.route.push([agent.nextX, agent.nextY])
+            updateQ(action, reward, qAry, agent, ALPHA, GAMMA)
+
+            // 現在地の更新
+            agent.x = agent.nextX
+            agent.y = agent.nextY
+
+            // ゴール判定
+            if (agent.x == MAZE_SIZE - 2 && agent.y == MAZE_SIZE - 2) {
+                isGoal = true
+            }
+        }
+
+        if (agent.step < minStep) {
+            minStep = agent.step
+            minRoute = agent.route
+        }
+        console.log("learn:" + i + ", stop:" + agent.step + ", min:" + minStep)
     }
 
-    if (agent.step < minStep) {
-        minStep = agent.step
-        minRoute = agent.route
-    }
-    console.log("learn:" + i + ", stop:" + agent.step + ", min:" + minStep)
+    console.log(minRoute)
+    shortestRoute(minRoute, 0, MAZE_ARRAY)
 }
-
-console.log(minRoute)
-shortestRoute(minRoute, 0, MAZE_ARRAY)
